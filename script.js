e08df5fe64568061cca17d2ec731c8e85eb739b3
kouame09 <princekouame7@gmail.com>
2023-10-16 15:23:54 +0000

    commit my app

---
+++ script.js
@@ -0,0 +1,28 @@
+const open = document.querySelector('.icon-menu');
+const nav = document.querySelector('.nav-bar');
+const close = document.querySelector('.main-container');
+const btnClose = document.querySelector('.close img');
+
+open.addEventListener('click', () =>{
+    nav.classList.add('translate-x');
+})
+
+btnClose.addEventListener('click', () =>{
+    nav.classList.remove('translate-x');
+})
+
+
+// Récupération des éléments nécessaires du DOM
+const imagePrincipale = document.querySelector('.image-product-1');
+const miniatures = document.querySelectorAll('.products-image');
+
+// Ajout d'un gestionnaire d'événement à chaque miniature d'image
+miniatures.forEach((miniature, index) => {
+  miniature.addEventListener('click', () => {
+    // Mettez à jour l'image principale avec la miniature cliquée
+    imagePrincipale.src = `./images/image-product-${index + 1}.jpg`;
+
+    // Changer le texte alternatif de l'image principale 
+    imagePrincipale.alt = `products-item${index + 1}`;
+  });
+});
